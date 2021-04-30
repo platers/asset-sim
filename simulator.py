888cf9b6bc2366facbdd17d8ff94ae47e56908a1
Platers <victao@bergen.org>
2021-04-29 22:29:58 -0400

    fix number of years off by one

--- simulator.py
+++ simulator.py
@@ -3,21 +3,18 @@ import numpy as np
 
 class Simulator:
     def simulate(self, years, strategies, runs=200):
-        means = []
-        lower = []
         columns = ['Year', 'Strategy', 'Assets', 'Run']
         data = []
         for strategy in strategies:
             for run in range(runs):
                 assets = strategy.assumptions.STARTING_AMOUNT
                 run_data = []
-                for year in range(years):
+                for year in range(years + 1):
+                    data.append([year, strategy.name, assets, run])
                     assets = strategy.annual_returns(year, assets)
                     assets += strategy.assumptions.annual_savings(year)
                     assets = max(assets, 1000) # bankruptcy
                     run_data.append(assets)
-                    tmp = [year, strategy.name, assets, run]
-                    data.append(tmp)
         
         df = pd.DataFrame(data, columns=columns)
         return df
